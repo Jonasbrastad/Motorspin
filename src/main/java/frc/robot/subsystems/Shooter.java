b67216e2da3cd4b2c6a0b4d5d6e3bfb2009410a4
Jonas Brastad <jonas.brastad@misd400.org>
2022-11-08 16:17:28 -0800

    First Commit!

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants;
+import static frc.robot.Constants.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+
+public class ExampleSubsystem extends SubsystemBase {
+  
+  public final  WPI_TalonFX shooty = new  WPI_TalonFX(shooterMotorID);
+  
+  public CommandBase stop() {
+
+    return new RunCommand(() -> shooty.set(0.1), this);
+
+  }
+}
+
+//Motor ID 5
